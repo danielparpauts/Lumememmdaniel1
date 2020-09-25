a8d64fb5c23cc2b3f12f86d61a7807348d256add
Opilane TTHK <opilane@tthk.ee>
2020-09-25 12:50:15 +0300

    Add project files.

---
+++ Lumememmdaniel1/Lumememmdaniel1/Lumememm.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lumememmdaniel1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Lumememm : ContentPage
+    {
+        public Lumememm()
+        {
+            InitializeComponent();
+        }
+
+        private void ON_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OFF_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Random_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Oma_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
